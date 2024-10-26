a8d815189efb0423fc7271c49435367cbd958ee7
huykingsofm <huykingsofm@gmail.com>
2024-10-27 05:31:28 +0700

    update

--- domain/oauth2_client.go
+++ domain/oauth2_client.go
@@ -1,12 +1,10 @@
 package domain
 
 import (
-	"github.com/todennus/x/scope"
 	"github.com/xybor-x/snowflake"
 )
 
 type OAuth2Client struct {
-	ID           snowflake.ID
-	Name         string
-	AllowedScope scope.Scopes
+	ID   snowflake.ID
+	Name string
 }
--- infras/service/grpc/conversion.go
+++ infras/service/grpc/conversion.go
@@ -4,7 +4,6 @@ import (
 	"github.com/todennus/oauth2-service/domain"
 	"github.com/todennus/proto/gen/service/dto/resource"
 	"github.com/todennus/shared/enumdef"
-	"github.com/todennus/shared/scopedef"
 	"github.com/todennus/x/enum"
 	"github.com/xybor-x/snowflake"
 )
@@ -20,8 +19,7 @@ func NewUser(user *resource.User) *domain.User {
 
 func NewOAuth2Client(client *resource.OAuth2Client) *domain.OAuth2Client {
 	return &domain.OAuth2Client{
-		ID:           snowflake.ID(client.Id),
-		Name:         client.Name,
-		AllowedScope: scopedef.Engine.ParseScopes(client.AllowedScope),
+		ID:   snowflake.ID(client.Id),
+		Name: client.Name,
 	}
 }
